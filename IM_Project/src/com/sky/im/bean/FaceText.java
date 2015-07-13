c551e7ef1f90bd0fd559195d412c23e266c2ff7c
PhoneChan <chenfeng905878199@gmail.com>
2015-07-13 18:06:00 +0800

    first_commit

---
+++ IM_Project/src/com/sky/im/bean/FaceText.java
@@ -0,0 +1,11 @@
+package com.sky.im.bean;
+
+public class FaceText {
+	public String text;
+
+	public FaceText(String text) {
+		super();
+		this.text = text;
+	}
+
+}
---
+++ IM_Project/src/com/sky/im/util/PinyinComparator.java
@@ -0,0 +1,20 @@
+package com.sky.im.util;
+
+import java.util.Comparator;
+
+import com.sky.im.bean.User;
+
+public class PinyinComparator implements Comparator<User> {
+
+	public int compare(User o1, User o2) {
+		if (o1.getSortLetters().equals("@")
+				|| o2.getSortLetters().equals("#")) {
+			return -1;
+		} else if (o1.getSortLetters().equals("#")|| o2.getSortLetters().equals("@")) {
+			return 1;
+		} else {
+			return o1.getSortLetters().compareTo(o2.getSortLetters());
+		}
+	}
+
+}
